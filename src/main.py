a99a5440627eae8246eb2e67cd5ed5d3d2267fa8
Safa Andac <8544658+SanD94@users.noreply.github.com>
2024-11-07 14:41:20 +0100

    add copy function for template

--- src/main.py
+++ src/main.py
@@ -1,17 +1,31 @@
-from textnode import TextNode, TextType
-from htmlnode import ParentNode, LeafNode
+import os
+import shutil
+from typing import TextIO
 
 def main():
-    node = ParentNode("p", [
-            LeafNode("b", "Bold text"),
-            LeafNode(None, "Normal text"),
-            LeafNode("i", "italic text"),
-            LeafNode(None, "Normal text"),
-            ])
-    print(node)
+    delete_files("public")
+    copy_files(src = "static", dest = "public")
     
 
-  
+def delete_files(src = "public"):
+    if os.path.exists(src):
+        shutil.rmtree(src)
+
+
+def copy_files(src = "static", dest = "public"):
+    if os.path.exists(src) == False:
+        raise ValueError(f"source folder not found {src}")
+    
+    os.mkdir(dest)
+    for path in os.listdir(src):
+        cur_path = os.path.join(src, path)
+        if os.path.isfile(cur_path):
+            shutil.copy(cur_path, dest)
+        else:
+            next_dest = os.path.join(dest, path)
+            copy_files(cur_path, next_dest)
+        
+
 
 if __name__ == "__main__":
     main()
